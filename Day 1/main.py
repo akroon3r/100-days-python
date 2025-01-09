cd5d765f0257533563a6dc5175a9e08e285b1764
Adam Kroon <adamkroon@Adams-Mac-mini.local>
2025-01-09 09:00:37 -0800

    completed day 1 exercise

---
+++ Day 1/main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("Hello!")
+    city = input("What City did you grow up in?\n")
+    pet_name = input("What is the name of a pet you own?\n")
+    band_name = city + " " + pet_name
+
+    print("Your band name is the " + band_name +"'s")
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
